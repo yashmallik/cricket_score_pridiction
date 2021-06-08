07f12867b3e37c77a68aadcb64307450e3ed5b32
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-06-08 22:43:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+### Imports ###
+# add imports - classes and defs
+import sys
+from predictor import predict_runs
+"""
+Created by Projjwal Mallik and team
+Team id - IITMBSC07845
+"""
+
+
+runs = predict_runs(testInput.csv)
+print(runs)
